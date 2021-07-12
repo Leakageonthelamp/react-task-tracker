e20fdf04e3008542ffd80ec167cb37a575231883
Leakageonthelamp <kittitat.chainonthi@gmail.com>
2021-07-13 05:01:39 +0700

    learn react

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,31 @@
+import PropTypes from "prop-types";
+import Button from "./Button";
+
 const Header = ({ title }) => {
-    return (
-        <header>
-            <h1>{title}</h1>
-        </header>
-    )
-}
+  const onClick = () => {
+    console.log('click');
+  };
+
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
+    </header>
+  );
+};
 
 Header.defaultProps = {
-    title: 'Task Tracker'
-}
+  title: "Task Tracker",
+};
 
-export default Header
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+// CSS style in JS file
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black'
+// }
+
+export default Header;
