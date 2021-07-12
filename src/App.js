d0312af04354551089c05a50ec6b376e8fbda17a
Leakageonthelamp <kittitat.chainonthi@gmail.com>
2021-07-13 04:25:56 +0700

    commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import Header from './components/Header'
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <Header title='Task Tracker' />
+    </div>
+  );
+}
+
+export default App;
+ 
